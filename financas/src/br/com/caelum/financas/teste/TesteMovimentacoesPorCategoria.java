ad4ae25c84370111a81a131d9723cb894855ceea
Douglas de Sousa Santos <dsousasantos91@gmail.com>
2020-01-29 00:36:37 -0300

    Concluir Aula VI - Modulo I

---
+++ financas/src/br/com/caelum/financas/teste/TesteMovimentacoesPorCategoria.java
@@ -0,0 +1,36 @@
+package br.com.caelum.financas.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.caelum.financas.modelo.Categoria;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteMovimentacoesPorCategoria {
+	
+	public static void main(String[] args) {
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+        em.getTransaction().begin();
+
+        Categoria categoria = new Categoria();
+        categoria.setId(1);
+
+        String jpql = "select m from Movimentacao m join m.categorias c where c = :pCategoria";
+
+        Query query = em.createQuery(jpql);
+        query.setParameter("pCategoria", categoria);
+
+        List<Movimentacao> resultados = query.getResultList();
+        
+        resultados.forEach(m -> {
+        	System.out.println(m.getDescricao());
+        	System.out.println(m.getConta().getId());
+        });
+		
+	}
+
+}
